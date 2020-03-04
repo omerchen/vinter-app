4f7e010b1296c3854be307e41827fdfba9d686d4
Omer Chen <omerchen15@gmail.com>
2020-03-05 01:37:18 +0200

    config basic default settings for navigation header bar

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,14 +1,45 @@
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-import FirstScreen from  '../screens/FirstScreen'
-import CreateFixtureScreen from  '../screens/CreateFixtureScreen'
-import PreviousFixturesScreen from  '../screens/PreviousFixturesScreen'
+import FirstScreen from "../screens/FirstScreen";
+import CreateFixtureScreen from "../screens/CreateFixtureScreen";
+import PreviousFixturesScreen from "../screens/PreviousFixturesScreen";
 
-const MainNavigator = createStackNavigator({
-    First: FirstScreen,
-    CreateFixture: CreateFixtureScreen,
-    PreviousFixtures: PreviousFixturesScreen
-});
+import Colors from "../constants/colors";
 
-export default createAppContainer(MainNavigator)
+const MainNavigator = createStackNavigator(
+  {
+    First: {
+      screen: FirstScreen,
+      navigationOptions: {
+        headerTitle: ""
+      }
+    },
+    CreateFixture: {
+      screen: CreateFixtureScreen,
+      navigationOptions: {
+        headerTitle: "מחזור חדש"
+      }
+    },
+    PreviousFixtures: {
+      screen: PreviousFixturesScreen,
+      navigationOptions: {
+        headerTitle: "מחזורים קודמים"
+      }
+    }
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Colors.primary
+      },
+      headerTintColor: Colors.white,
+      headerTitleStyle: {
+        fontFamily: "assistant-semi-bold"
+      },
+      headerTitleAlign: 'center'
+    }
+  }
+);
+
+export default createAppContainer(MainNavigator);
