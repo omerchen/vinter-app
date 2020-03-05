bae5e54dbf5cf8cd6fae7c33215d06c5385553f8
Omer Chen <omerchen15@gmail.com>
2020-03-05 12:04:13 +0200

    add button to header bar

--- screens/AllPlayersScreen.js
+++ screens/AllPlayersScreen.js
@@ -1,31 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import CommonStyles from '../constants/common-styles'
-import Colors from '../constants/colors'
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Colors from "../constants/colors";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
 
 let AllPlayersScreen = props => {
-  let notPlayersExistsView = <Text style={styles.nonPlayersTitle}>לא קיימים שחקנים כרגע במערכת</Text>
-  let playersListView = null
+  let notPlayersExistsView = (
+    <Text style={styles.nonPlayersTitle}>לא קיימים שחקנים כרגע במערכת</Text>
+  );
+  let playersListView = null;
   return (
     <View style={styles.container}>
-      {props.navigation.getParam("players").length > 0?playersListView:notPlayersExistsView}
+      {props.navigation.getParam("players").length > 0
+        ? playersListView
+        : notPlayersExistsView}
     </View>
   );
-}
+};
+
+AllPlayersScreen.navigationOptions = navigationData => {
+  console.log(navigationData);
+  return {
+    headerRight: ()=>{
+      return <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add Player"
+          iconName="ios-add"
+          onPress={() => {
+            console.log("Clicked! :)");
+          }}
+        />
+      </HeaderButtons>
+    }
+  };
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.white,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
   nonPlayersTitle: {
     fontSize: 20,
-    fontFamily: 'assistant-semi-bold',
+    fontFamily: "assistant-semi-bold",
     color: Colors.darkGray
   }
 });
 
-export default AllPlayersScreen
+export default AllPlayersScreen;
