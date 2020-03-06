fd437073983aa1c1a204c9e367dc133e62adf86d
Omer Chen <omerchen15@gmail.com>
2020-03-06 11:15:08 +0200

    add minimal support for iphones

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { Text, StyleSheet, View, TouchableNativeFeedback } from "react-native";
+import { Text, StyleSheet, View, TouchableNativeFeedback, Platform } from "react-native";
 import Colors from "../constants/colors";
 import Sizes from "../constants/sizes";
+import SubButton from './SubButton'
 
 let MainButton = props => {
+  if (Platform.OS == "ios")
+  {
+    return <SubButton style={{fontFamily: "assistant-extra-bold"}} {...props}/>
+  }
 
   return (
     <View style={{...styles.wrapper, ...props.style, width:props.width?props.width:420, backgroundColor: props.offline?Colors.darkGray:Colors.primary}}>
