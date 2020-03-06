9e7c7f60c122730a3d79642685eca786bb32cce2
Omer Chen <omerchen15@gmail.com>
2020-03-07 00:04:14 +0200

    wrap parse player

--- helpers/fixture-list-parser.js
+++ helpers/fixture-list-parser.js
@@ -24,7 +24,17 @@ let determineColor = teamString => {
   let countBlue = occurrences(teamString, blueSign);
   let countOrange = occurrences(teamString, orangeSign);
   let countGreen = occurrences(teamString, greenSign);
-  console.log("blue("+countBlue+")"+" orange("+countOrange+")"+ " green("+countGreen+")")
+  console.log(
+    "blue(" +
+      countBlue +
+      ")" +
+      " orange(" +
+      countOrange +
+      ")" +
+      " green(" +
+      countGreen +
+      ")"
+  );
 
   if (countBlue + countOrange + countGreen !== 1) return null;
 
@@ -38,46 +48,64 @@ let determineColor = teamString => {
   return null;
 };
 
+let parsePlayer = (playerString, isCaptain = false) => {};
+
 let parseTeam = teamString => {
-  let team = {teamColorCode: determineColor(teamString)}
+  let lines = teamString.split("\n");
+
+  // validate number of players at a team
+  if (lines.length != 6 /*num of players + header line*/) {
+    Alert.alert("על כל הקבוצות להיות בפורמט של שורת כותרת ו5 שורות לשחקנים");
+    return null;
+  }
+
+  let team = { teamColorCode: determineColor(lines[0]), players: [] };
 
   // validate color
   if (team.teamColorCode == null) {
-    Alert.alert("לא ניתן לקבוע את צבע אחת הקבוצות")
-    return null
+    Alert.alert("לא ניתן לקבוע את צבע אחת הקבוצות");
+    return null;
   }
 
-  let gkIdentifier = "(שוער)"
+  let gkIdentifier = "(שוער)";
 
   // validate maximum number of gks
   if (occurrences(teamString, gkIdentifier) > 1) {
-
-    Alert.alert("בקבוצה מסויימת יש יותר משוער אחד")
-    return null
+    Alert.alert("בקבוצה מסויימת יש יותר משוער אחד");
+    return null;
   }
 
-  return team
+  for (let i in lines) {
+    if (i === 0) continue;
+
+    let parsedPlayer = parsePlayer(lines[i], i === 1);
+
+    if (parsedPlayer == null) return null;
+
+    team.players.push(parsedPlayer);
+  }
+
+  return team;
 };
 
-let occurrences = (string, subString, allowOverlapping=true) => {
-
+let occurrences = (string, subString, allowOverlapping = true) => {
   string += "";
   subString += "";
-  if (subString.length <= 0) return (string.length + 1);
+  if (subString.length <= 0) return string.length + 1;
 
   var n = 0,
-      pos = 0,
-      step = allowOverlapping ? 1 : subString.length;
+    pos = 0,
+    step = allowOverlapping ? 1 : subString.length;
 
   while (true) {
-      pos = string.indexOf(subString, pos);
-      if (pos >= 0) {
-          ++n;
-          pos += step;
-      } else break;
+    pos = string.indexOf(subString, pos);
+    if (pos >= 0) {
+      ++n;
+      pos += step;
+    } else break;
   }
   return n;
-}
+};
 
 export default fixtureList => {
   // not empty
@@ -104,15 +132,15 @@ export default fixtureList => {
     return null;
   }
 
-  let parsedList = []
+  let parsedList = [];
 
   for (let i in preParseTeams) {
-    let currentParsedTeam = parseTeam(preParseTeams[i])
+    let currentParsedTeam = parseTeam(preParseTeams[i]);
 
     if (currentParsedTeam == null) {
-      return null
+      return null;
     } else {
-      parsedList.push(currentParsedTeam)
+      parsedList.push(currentParsedTeam);
     }
   }
 
