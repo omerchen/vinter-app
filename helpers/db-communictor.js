1aa17acb982a26c2f6bceb13be4d7cab12018da6
Omer Chen <omerchen15@gmail.com>
2020-03-07 02:42:14 +0200

    create debug flag for database communicator

--- helpers/db-communictor.js
+++ helpers/db-communictor.js
@@ -1,6 +1,7 @@
 import Axios from 'axios'
 
 class DBCommunicator {
+    isDebug = true // TODO: use this flag for switching between databases
     url = 'https://vinter-app.firebaseio.com/'
     getPlayers = () => Axios.get(this.url+'players.json')
     setPlayers = (players) => Axios.put(this.url+'players.json', players)
