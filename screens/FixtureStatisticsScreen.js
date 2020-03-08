0fbe1d9583a6b87ffb88d5a2c09f3d1ed0982d0e
Omer Chen <omerchen15@gmail.com>
2020-03-08 18:14:35 +0200

    set up table for statistics

--- screens/FixtureStatisticsScreen.js
+++ screens/FixtureStatisticsScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import { StyleSheet, Text, View, ColorPropType } from "react-native";
 import Colors from "../constants/colors";
 import { useSelector } from "react-redux";
+import { Table, Row, Rows } from "react-native-table-component";
+import { ScrollView } from "react-native-gesture-handler";
 
 let FixtureStatisticsScreen = props => {
   const fixtures = useSelector(state => state.fixtures);
@@ -9,6 +11,31 @@ let FixtureStatisticsScreen = props => {
   const fixture = fixtures[fixtureId];
   const matches = fixture.matches ? fixture.matches : [];
   const closedMatches = matches.filter(item => !item.isRemoved && !item.isOpen);
+
+  const tableHead = ["Head", "Head2", "Head3", "Head4"];
+  const tableData = [
+    ["1", "2", "3", "4"],
+    ["a", "b", "c", "d"],
+    ["a", "b", "c", "d"],
+    ["1", "2", "3", "4"],
+    ["a", "b", "c", "d"],
+    ["a", "b", "c", "d"],
+    ["1", "2", "3", "4"],
+    ["a", "b", "c", "d"],
+    ["a", "b", "c", "d"],
+    ["1", "2", "3", "4"],
+    ["a", "b", "c", "d"],
+    ["a", "b", "c", "d"],
+    ["1", "2", "3", "4"],
+    ["a", "b", "c", "d"],
+    ["a", "b", "c", "d"],
+    ["1", "2", "3", "4"],
+    ["a", "b", "c", "d"],
+    ["a", "b", "c", "d"],
+    ["1", "2", "3", "4"],
+    ["a", "b", "c", "d"],
+    ["a", "b", "c", "d"],
+  ];
 
   if (closedMatches.length === 0) {
     return (
@@ -19,22 +46,33 @@ let FixtureStatisticsScreen = props => {
   }
 
   // STATISTICS CALCULATIONS STARTS HERE (closedMatches.length > 0)
-  
+
   return (
-    <View style={styles.container}>
-      <Text>
-        This is the FixtureStatisticsScreen screen! ({closedMatches.length})
-      </Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.tableTitle}>נתונים אישיים</Text>
+      <Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
+        <Row data={tableHead} style={styles.head} textStyle={styles.text} />
+        <Rows data={tableData} textStyle={styles.text} />
+      </Table>
+      <View style={{height:50}}/>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  head: { height: 40, backgroundColor: "#f1f8ff" },
+  text: { margin: 6 },
   container: {
     flex: 1,
+    padding: 16,
+    paddingTop: 30,
     backgroundColor: Colors.white,
-    alignItems: "center",
-    justifyContent: "center"
+  },
+  tableTitle: {
+    fontFamily: "assistant-semi-bold",
+    fontSize:30,
+    textAlign:"center",
+    marginBottom:20
   }
 });
 
