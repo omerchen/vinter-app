6b91cc94456073956ecf981d1a6aaa5eca5aaedc
Omer Chen <omerchen15@gmail.com>
2020-03-15 21:17:15 +0200

    implement sort for dates in transactions screen

--- screens/PlayerTransactionsScreen.js
+++ screens/PlayerTransactionsScreen.js
@@ -6,7 +6,7 @@ import {
   ScrollView,
   FlatList,
   Dimensions,
-  Platform,
+  Platform
 } from "react-native";
 import Colors from "../constants/colors";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
@@ -16,20 +16,33 @@ import TransactionCard from "../components/TransactionCard";
 import TextInput from "react-native-material-textinput";
 import { DismissKeyboardView } from "../components/DismissKeyboardView";
 import { SECURE_LEVEL_ADMIN } from "../constants/security-levels";
+import {mergeSort} from "../helpers/mergeSort"
 
 let PlayerTransactionsScreen = props => {
   let players = useSelector(state => state.players);
-  const playerId = props.navigation.getParam("playerId")
-  let player = players[playerId]
-  let transactions = player.transactions?player.transactions.filter(t=>!t.isRemoved):[]
+  const playerId = props.navigation.getParam("playerId");
+  let player = players[playerId];
+  let transactions = player.transactions
+    ? player.transactions.filter(t => !t.isRemoved)
+    : [];
 
   let notTransactionsExistsView = (
-    <View
-      style={{ justifyContent: "center", flex: 1, width:"100%"}}
-    >
+    <View style={{ justifyContent: "center", flex: 1, width: "100%" }}>
       <Text style={styles.nonPlayersTitle}>לא נמצאו טרנזקציות במערכת</Text>
     </View>
   );
+
+  const sortByDateString = (a, b) => {
+    let aArr = a.date.split(".").reverse();
+    let bArr = b.date.split(".").reverse();
+
+    for (let i in aArr) {
+      if (aArr[i] > bArr[i]) return true;
+      else if (bArr[i] > aArr[i]) return false;
+    }
+
+    return true
+  };
 
   let numOfColumn = 1;
   let windowWidth = Dimensions.get("window").width;
@@ -39,10 +52,14 @@ let PlayerTransactionsScreen = props => {
     <FlatList
       style={{ width: "100%" }}
       contentContainerStyle={{ alignItems: "center" }}
-      data={transactions}
+      data={Platform.OS=="web"?mergeSort(transactions, sortByDateString):transactions.sort(sortByDateString)}
       keyExtractor={item => item.id.toString()}
       renderItem={({ item }) => (
-        <TransactionCard playerId={playerId} transactionId={item.id} navigation={props.navigation} />
+        <TransactionCard
+          playerId={playerId}
+          transactionId={item.id}
+          navigation={props.navigation}
+        />
       )}
       numColumns={numOfColumn}
     />
@@ -53,25 +70,27 @@ let PlayerTransactionsScreen = props => {
     playerTransactionsSum += transactions[i].sum;
   }
 
-  playerTransactionsSum = playerTransactionsSum.toFixed(1)
-
+  playerTransactionsSum = playerTransactionsSum.toFixed(1);
 
   return (
     <View style={styles.container}>
-      <View style={styles.totalView}><Text style={styles.totalSubtitle}>מאזן כללי</Text>
-<Text style={styles.totalTitle}>{playerTransactionsSum+"₪"}</Text></View>
-      {transactions.length > 0 ? transactionsListView : notTransactionsExistsView}
+      <View style={styles.totalView}>
+        <Text style={styles.totalSubtitle}>מאזן כללי</Text>
+        <Text style={styles.totalTitle}>{playerTransactionsSum + "₪"}</Text>
+      </View>
+      {transactions.length > 0
+        ? transactionsListView
+        : notTransactionsExistsView}
     </View>
   );
 };
 
 PlayerTransactionsScreen.navigationOptions = navigationData => {
   return {
-    headerTitle:
-      "מאזן כספי",
+    headerTitle: "מאזן כספי",
     headerRight: () => {
-      if (Platform.OS == "web") return null
-      
+      if (Platform.OS == "web") return null;
+
       return (
         <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
           <Item
@@ -82,8 +101,10 @@ PlayerTransactionsScreen.navigationOptions = navigationData => {
                 routeName: "RequirePassword",
                 params: {
                   routeName: "AddTransaction",
-                  params: {playerId: navigationData.navigation.getParam("playerId"),},
-                  level: SECURE_LEVEL_ADMIN,
+                  params: {
+                    playerId: navigationData.navigation.getParam("playerId")
+                  },
+                  level: SECURE_LEVEL_ADMIN
                 }
               });
             }}
@@ -104,21 +125,21 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontFamily: "assistant-semi-bold",
     color: Colors.darkGray,
-    textAlign:"center"
+    textAlign: "center"
   },
   totalTitle: {
     fontFamily: "assistant-bold",
-    fontSize: 45,
+    fontSize: 45
   },
   totalSubtitle: {
-    marginTop:10,
+    marginTop: 10,
     fontFamily: "assistant-semi-bold",
     fontSize: 20,
-    color:Colors.black,
-    marginBottom:-10,
+    color: Colors.black,
+    marginBottom: -10
   },
   totalView: {
-    alignItems:"center"
+    alignItems: "center"
   }
 });
 
