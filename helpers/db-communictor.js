20b84b0c237f87af45ca00a6546ca9c9a1bc86b3
Omer Chen <omerchen15@gmail.com>
2020-03-16 11:57:06 +0200

    complete building backup db for the data

--- helpers/db-communictor.js
+++ helpers/db-communictor.js
@@ -1,24 +1,73 @@
-import Axios from 'axios'
-import { isDebug } from "../constants/configs"
+import Axios from "axios";
+import { isDebug } from "../constants/configs";
 
 class DBCommunicator {
-    url = 'https://vinter-app.firebaseio.com/'
-    playersTableName = 'players'
-    fixturesTableName = isDebug?'fixturesDebug':'fixturesprepilot'
-    visitsTableName = isDebug?'visitsDebug':'visits'
+  url = "https://vinter-app.firebaseio.com/";
+  backupUrl = "https://vinter-app-back-up.firebaseio.com/";
+  playersTableName = "players";
+  fixturesTableName = isDebug ? "fixturesDebug" : "fixturesprepilot";
+  visitsTableName = isDebug ? "visitsDebug" : "visits";
 
-    getPlayers = () => Axios.get(this.url+this.playersTableName+'.json')
-    setPlayers = (players) => Axios.put(this.url+this.playersTableName+'.json', players)
+  getPlayers = () => {
+    return Axios.get(this.url + this.playersTableName + ".json");
+  };
+  setPlayers = players => {
+    Axios.put(this.backupUrl + this.playersTableName + ".json", players).then(
+      res => {
+        if (res.status != 200) {
+          console.log("backup failed..");
+        }
+      }
+    );
+    return Axios.put(this.url + this.playersTableName + ".json", players);
+  };
 
-    getVisits = () => Axios.get(this.url+this.visitsTableName+'.json')
-    setVisits = (visits) => Axios.put(this.url+this.visitsTableName+'.json', visits)
+  getVisits = () => {
+    return Axios.get(this.url + this.visitsTableName + ".json");
+  };
+  setVisits = visits => {
+    Axios.put(this.backupUrl + this.visitsTableName + ".json", visits).then(
+      res => {
+        if (res.status != 200) {
+          console.log("backup failed..");
+        }
+      }
+    );
+    return Axios.put(this.url + this.visitsTableName + ".json", visits);
+  };
 
-    getFixtures = () => Axios.get(this.url+this.fixturesTableName+'.json')
-    setFixtures = (fixtures) => Axios.put(this.url+this.fixturesTableName+'.json', fixtures)
+  getFixtures = () => {
+    return Axios.get(this.url + this.fixturesTableName + ".json");
+  };
 
-    getFixture = (fixtureId) => Axios.get(this.url+this.fixturesTableName+'/'+fixtureId+'.json')
-    
-    getMatch = (fixtureId, matchId) => Axios.get(this.url+this.fixturesTableName+'/'+fixtureId+'/matches/'+matchId+'.json')
+  setFixtures = fixtures => {
+    Axios.put(this.backupUrl + this.fixturesTableName + ".json", fixtures).then(
+      res => {
+        if (res.status != 200) {
+          console.log("backup failed..");
+        }
+      }
+    );
+    return Axios.put(this.url + this.fixturesTableName + ".json", fixtures);
+  };
+
+  getFixture = fixtureId => {
+    return Axios.get(
+      this.url + this.fixturesTableName + "/" + fixtureId + ".json"
+    );
+  };
+
+  getMatch = (fixtureId, matchId) => {
+    return Axios.get(
+      this.url +
+        this.fixturesTableName +
+        "/" +
+        fixtureId +
+        "/matches/" +
+        matchId +
+        ".json"
+    );
+  };
 }
 
-export default new DBCommunicator()
+export default new DBCommunicator();
