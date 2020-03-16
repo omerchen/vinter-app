1371783c9945a347cf9fc04740aaf52ea7a774ac
Omer Chen <omerchen15@gmail.com>
2020-03-16 11:34:50 +0200

    update version to 2.1.0

--- constants/configs.js
+++ constants/configs.js
@@ -1,6 +1,6 @@
 // Deployment Configurations
 export const isDebug = true
-export const version = "2.0.1"
+export const version = "2.1.0"
 
 // UX Configurations
 export const timeToVibrate= [6*60,7*60,8*60,9*60] // minutes: 6,7,8,9
