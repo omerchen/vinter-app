bbcafcf14d456ae1179cd9768cbfe702e1c5f115
Omer Chen <omerchen15@gmail.com>
2021-04-10 12:15:34 +0300

    change the cost to 30

--- constants/configs.js
+++ constants/configs.js
@@ -1,6 +1,6 @@
 // Deployment Configurations
 export const isDebug = false
-export const version = "3.4.7"
+export const version = "3.4.8"
 
 // UX Configurations
 export const timeToVibrate= [6*60,7*60,8*60,9*60] // minutes: 6,7,8,9
@@ -8,5 +8,5 @@ export const vibrateDuration= 5*1000 // 5 Seconds
 export const liveRequestInterval = 3000 // every 3 seconds
 export const liveSessionTime = 120 * 60 * 1000; // 120 minutes
 export const maxLiveRequests = liveSessionTime / liveRequestInterval
-export const soldierCost = 15
-export const standardCost = 25
+export const soldierCost = 30
+export const standardCost = 30
